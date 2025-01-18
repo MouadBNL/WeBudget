4b8bec75324159f5fde259a7631bc5f6bb346744
MouadBNL <mouadbnl.2k01@gmail.com>
2025-01-18 22:11:13 +0100

    feat: auth layout

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Blog() {
+  return (
+    <div>
+      <p>Blog page</p>
+      <Link href="/">home</Link>
+    </div>
+  );
+}
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import * as React from "react";
+import {
+  ArrowRightLeft,
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  LayoutDashboard,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+  User,
+  Wallet,
+} from "lucide-react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavProjects } from "@/components/nav-projects";
+import { NavUser } from "@/components/nav-user";
+import { TeamSwitcher } from "@/components/team-switcher";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/auth/dashboard",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "Transactions",
+      url: "/auth/transactions",
+      icon: ArrowRightLeft,
+    },
+    {
+      title: "Budget",
+      url: "/auth/budget",
+      icon: Wallet,
+    },
+    {
+      title: "Chat",
+      url: "/auth/chat",
+      icon: Bot,
+    },
+  ],
+  settings: [
+    {
+      name: "Account",
+      url: "#",
+      icon: User,
+    },
+    {
+      name: "Settings",
+      url: "#",
+      icon: Settings2,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavProjects projects={data.settings} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
